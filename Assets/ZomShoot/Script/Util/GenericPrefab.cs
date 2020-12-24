7d19b880970a6ed8e4029ad1dc0f1af17da40a44
haiun <gewdox@naver.com>
2020-12-25 01:16:44 +0900

    폴더정리 & UI관련 리소스 추가.

---
+++ Assets/ZomShoot/Script/Util/GenericPrefab.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+public class PrefabPath : Attribute
+{
+    public string Path { get; set; }
+    public PrefabPath(string path)
+    {
+        Path = path;
+    }
+}
+
+public class GenericPrefab
+{
+    public static T Instantiate<T>()
+    {
+        var prefabPathAttr = typeof(T).GetCustomAttribute<PrefabPath>();
+        var prefab = Resources.Load(prefabPathAttr.Path) as GameObject;
+        var go = GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity);
+        return go.GetComponent<T>();
+    }
+
+    public static T Instantiate<T>(Transform parent)
+    {
+        var prefabPathAttr = typeof(T).GetCustomAttribute<PrefabPath>();
+        var prefab = Resources.Load(prefabPathAttr.Path) as GameObject;
+        var go = GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity, parent);
+        return go.GetComponent<T>();
+    }
+}
