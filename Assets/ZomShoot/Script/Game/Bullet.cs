f21442f6cce23cf8d047efee69f9b1c743bfd414
haiun <gewdox@naver.com>
2020-12-27 19:06:59 +0900

    탄환 오브젝트 변경 & 피격효과음 적용 스테이지 이동간 로직 추가 준비.

---
+++ Assets/ZomShoot/Script/Game/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody RigidBody = null;
+
+    public float Speed = 2500f;
+
+    private void Start()
+    {
+        RigidBody.AddForce(transform.forward * Speed, ForceMode.Force);
+
+        StartCoroutine(Disapper());
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Met")
+        {
+            GameInstance.Inst?.PlaySfx(SfxEnum.BulletHit_Metal);
+        }
+        else if (collision.gameObject.tag == "Con")
+        {
+            GameInstance.Inst?.PlaySfx(SfxEnum.BulletHit_Con);
+        }
+        else
+        {
+            GameInstance.Inst?.PlaySfx(SfxEnum.BulletHit_Dirt);
+        }
+        Destroy(gameObject);
+    }
+
+    IEnumerator Disapper()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
+}
